9422f8d51691cc0ef63a1990c7f849789b3ee17a
belchushka <savvashu@gmail.com>
2023-03-03 02:08:09 +0300

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hashbone_test/",
+  base: "/hashbon_test/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
